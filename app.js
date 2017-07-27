1b7ea603e8dbeba3e343c48db05f13bfcc684cda
Natalia Silva <nataliasilva.contato@hotmail.com>
2017-07-27 18:59:14 -0300

    erro de visualização consertado

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-
-
-app.get('/', function(req,res){
-    res.send("<html><title>♥Valentine's</title></title><body><h1>Natalia Silva</h1></body></html>");
-});
-
-app.get('/moda', function(req,res){
+app.get('/moda', function(req, res){
     res.render("secao/moda");
 });
 
-app.get('/vlogs', function(req,res){
+app.get('/vlogs', function(req, res){
     res.render("secao/vlogs");
+});
+
+app.get('/', function(req, res){
+    res.send("<html><title>♥Valentine's</title></title><body><h1>Natalia Silva</h1></body></html>");
 });
 
 app.listen(3000, function(){
