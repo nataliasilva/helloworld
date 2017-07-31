3f23e8850f552b5056aa4d3c4511f4f807212458
Natalia Silva <nataliasilva.contato@hotmail.com>
2017-07-31 00:00:49 -0300

    criando conexão com o banco

--- app/routs/noticias.js
+++ app/routs/noticias.js
@@ -1,5 +1,19 @@
-module.exports = function(app){
-    app.get('/noticias', function(req, res) {
-        res.render("noticias/noticias");
+module.exports = function (app) {
+    
+    app.get('/noticias', function (req, res) {
+    
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function (error, result) {
+            res.render("noticias/noticias", {noticias : result});
+        });
+ 
     });
 };
